5244c180bb1eaa34aefe9181d4bf9f9b68479b20
Piotrek <ptrlechowicz@gmail.com>
2022-11-06 20:51:25 +0100

    deleted Menu from Page component

--- src/Containers/Page/Page.js
+++ src/Containers/Page/Page.js
@@ -1,22 +1,20 @@
-import Header from "../Header/Header";
-import Layout from "../Layout/Layout";
-import Footer from "../Footer/Footer";
-import Menu from "../Menu/Menu";
+import { Link } from 'react-router-dom';
+import styles from './Page.module.css';
+import withClass from '../../hoc/withClass';
 
 const Page = props => {
-  const header = <Header title={props.title} />;
-  const menu = <Menu />
-  const content = props.content;
-  const footer = <Footer />
-
   return (
-    <Layout
-      header={header}
-      menu={menu}
-      content={content}
-      footer={footer}
-    />
+    <div>
+      <h2 className={styles.header}>{props.title}</h2>
+      <img src={props.src} alt={props.alt} />
+      <div className={styles.sentences}>
+        <p className={styles.sentence}>{props.firstSentence}</p>
+        <p className={styles.sentence}>{props.secondSentence}</p>
+        <p className={styles.sentence}>{props.thirdSentence}</p>
+      </div>
+      <Link to='/'>Powrót</Link>
+    </div>
   )
 }
 
-export default Page;
+export default withClass(Page, 'wrapper');

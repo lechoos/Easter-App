5f9b52e2f12677270c4cfcf0fc651f28bd9bb46d
Piotrek <ptrlechowicz@gmail.com>
2022-11-06 20:53:06 +0100

    deleted menu from layout

--- src/Containers/Layout/Layout.js
+++ src/Containers/Layout/Layout.js
@@ -2,6 +2,7 @@ const Layout = props => {
   return (
     <>
       <div>{props.header}</div>
+      <div>{props.menu}</div>
       <div className='content'>{props.content}</div>
       <div>{props.footer}</div>
     </>

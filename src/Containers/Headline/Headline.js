a0d348f00e26d67add6c094236422aca815e640f
Piotrek <ptrlechowicz@gmail.com>
2022-11-07 20:43:05 +0100

    styled Headline

--- src/Containers/Headline/Headline.js
+++ src/Containers/Headline/Headline.js
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import ReducerContext from "../../context/reducerContext";
+import styles from './Headline.module.css';
 
 const Headline = () => {
   const { state } = useContext(ReducerContext);
 
   return (
-    <h3>W {state.year}:</h3>
+    <h3 className={styles.header}>W roku {state.year}:</h3>
   )
 }
 

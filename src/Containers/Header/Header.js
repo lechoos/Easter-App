53e875b1b50c1b62c30a0c797cbd05274fff67b0
Piotrek <ptrlechowicz@gmail.com>
2022-11-12 15:16:49 +0100

    created a Color Button

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -1,9 +1,11 @@
+import ColorButton from '../../Components/ColorButton/ColorButton';
 import styles from './Header.module.css';
 
 const Header = (props) => {
 	return (
 		<header className={styles.header}>
 			<h1>Easter App</h1>
+			<ColorButton />
 			<div className={styles.shadow}></div>
 		</header>
 	);

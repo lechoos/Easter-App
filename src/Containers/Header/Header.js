4df41f9e2933170bd43d5da63e24fd5ac41258f4
Piotrek <ptrlechowicz@gmail.com>
2022-11-15 23:33:45 +0100

    deleted a color button from Header

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -1,11 +1,9 @@
-import ColorButton from '../../Components/ColorButton/ColorButton';
 import styles from './Header.module.css';
 
-const Header = (props) => {
+const Header = () => {
 	return (
 		<header className={styles.header}>
 			<h1>Easter App</h1>
-			<ColorButton />
 			<div className={styles.shadow}></div>
 		</header>
 	);

8118aa5b710e568b070f1f6cdd261d8981187c06
Piotrek <ptrlechowicz@gmail.com>
2023-01-09 15:08:49 +0100

    deleted unnecessary code

--- src/Components/ColorButton/ColorButton.js
+++
@@ -1,9 +0,0 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPalette } from '@fortawesome/free-solid-svg-icons';
-import styles from './ColorButton.module.css';
-
-const ColorButton = () => (
-  <button className={styles.colorBtn}><FontAwesomeIcon icon={faPalette} /></button>
-)
-
-export default ColorButton;

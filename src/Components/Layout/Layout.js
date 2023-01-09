83ebac4e6b004436dbc16dd8c9ec3a79cc71e064
Piotrek <ptrlechowicz@gmail.com>
2023-01-09 16:14:52 +0100

    deleted unnecesary code

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -2,7 +2,6 @@ const Layout = (props) => {
 	return (
 		<>
 			<div>{props.header}</div>
-			<div>{props.menu}</div>
 			<div className='content'>{props.content}</div>
 			<div>{props.footer}</div>
 		</>
